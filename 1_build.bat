d84ef70fdb13665563191ffaed53760878594071
Dmitriy Donskoy <sentike161rus@gmail.com>
2023-04-16 03:11:22 +0300

    Added support for using precompiled libs

--- 1_build.bat
+++ 1_build.bat
@@ -1,5 +1,9 @@
 "scons" p=windows target=editor arch="x86_64" debug_symbols="yes" builtin_embree="no" builtin_libvorbis="no" builtin_libogg="no" builtin_libtheora="no" builtin_libwebp="no"  use_llvm="yes" optimize="speed_trace" vsproj="yes" compiledb="yes" openxr="no" opengl3="no" deprecated="no" module_arkit_enabled="no" module_mobile_vr_enabled="no" module_mono_enabled="no" module_webxr_enabled="no" module_webrtc_enabled="no" module_websocket_enabled="no" -j 64
- 
+
+
+"scons" p=windows target=editor arch="x86_64" debug_symbols="yes" builtin_embree="no" builtin_libvorbis="no" builtin_libogg="no" builtin_libtheora="no" builtin_libwebp="no"  use_llvm="yes" optimize="speed_trace" vsproj="yes" compiledb="no" openxr="no" opengl3="no" deprecated="no" module_arkit_enabled="no" module_mobile_vr_enabled="no" module_mono_enabled="no" module_webxr_enabled="no" module_webrtc_enabled="no" module_websocket_enabled="no" use_static_cpp="no" builtin_enet="no" builtin_glslang="no" builtin_squish="no" builtin_zstd="no" builtin_libpng="no" -j 64
+
+
 bin/godot.windows.editor.dev.x86_64.mono.exe --generate-mono-glue modules/mono/glue
 python "./modules/mono/build_scripts/build_assemblies.py" --godot-output-dir=./bin --godot-platform=windows
 
